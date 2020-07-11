5e84a7df8556273075ed0cb3abba2c092ccb2aa2
tdamron <TristanDamron31@gmail.com>
2020-07-11 02:54:45 -0700

    Added a black out animation (no respawn)

---
+++ Assets/Scripts/Blackout.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blackout : MonoBehaviour
+{
+    [SerializeField]
+    private Image _blackOutPanel;
+
+    void Update()
+    {
+        if (GameManager.blackOut) {
+            _blackOutPanel.color = Color.Lerp(_blackOutPanel.color, Color.black, Time.deltaTime * 2f);
+        }
+    }
+}
