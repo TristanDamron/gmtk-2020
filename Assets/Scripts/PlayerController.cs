25367b19fbf5b9a2b4d637c8c0df4efacdb3b7a1
tdamron <TristanDamron31@gmail.com>
2020-07-10 19:47:12 -0700

    Add PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _playerSpeed;
+    [SerializeField]
+    private float _maxSpeed;
+    [SerializeField]
+    private float _accelerationRate;
+    [SerializeField]
+    private float _decellerationRate;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move() {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        var moving = (horizontal != 0f || vertical != 0f);
+        
+        if (moving) {
+            Accelerate();            
+            var pos = transform.position;
+            pos.x += horizontal;
+            pos.y += vertical;
+            transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * _playerSpeed);
+        } else {
+            Decellerate();
+        }
+    }
+
+    private void Accelerate() {
+        if (_playerSpeed <= _maxSpeed) {
+            _playerSpeed += _accelerationRate;
+        }
+    }
+
+    private void Decellerate() {
+        if (_playerSpeed >= 0f) {
+            _playerSpeed -= _decellerationRate;
+        }
+
+        if (_playerSpeed < 0f) {
+            _playerSpeed = 0f;
+        }
+    }
+}
