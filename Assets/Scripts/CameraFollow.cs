cb10b9bdc115f1599d280b5fad0089bc5b985f44
tdamron <TristanDamron31@gmail.com>
2020-07-10 23:23:20 -0700

    Add MainCamera prefab and CameraFollow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private float _delay;
+
+    void Start() {
+        if (!_target) {
+            _target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    void Update()
+    {
+        var pos = transform.position;
+        pos.x = _target.position.x;
+        pos.y = _target.position.y;
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime / _delay);
+    }
+}
