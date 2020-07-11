b38535e7872887644b2bd348da15977a0f1e07b2
tdamron <TristanDamron31@gmail.com>
2020-07-11 01:30:32 -0700

    Camera zoom mechanic

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,11 +8,18 @@ public class CameraFollow : MonoBehaviour
     private Transform _target;
     [SerializeField]
     private float _delay;
+    [SerializeField]
+    private float _minZoomLevel;
+    [SerializeField]
+    private float _maxZoomLevel;
 
     void Start() {
         if (!_target) {
             _target = GameObject.FindGameObjectWithTag("Player").transform;
         }
+        
+        _minZoomLevel = GetComponent<Camera>().orthographicSize;
+        _maxZoomLevel = _minZoomLevel * 4;
     }
 
     void Update()
@@ -21,5 +28,11 @@ public class CameraFollow : MonoBehaviour
         pos.x = _target.position.x;
         pos.y = _target.position.y;
         transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime / _delay);
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, _maxZoomLevel, Time.deltaTime * 4f);
+        } else {
+            GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, _minZoomLevel, Time.deltaTime * 4f);
+        }
     }
 }
