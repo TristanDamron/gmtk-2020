e0a35323bef9d9b2f023931c85d3315c77ba46e3
tdamron <TristanDamron31@gmail.com>
2020-07-10 21:58:12 -0700

    Ghost follows player

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GhostController : MonoBehaviour
-{
-    // Start is called before the first frame update
+{    
+    enum AIState {
+        StalkingPlayer,
+        BackToStart
+    }
+
+    [SerializeField]
+    private Transform _target;    
+    [SerializeField]
+    private float _moveSpeed;
+    [SerializeField]
+    private AIState _state;
+
     void Start()
-    {
-        
+    {        
+        _state = AIState.StalkingPlayer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_state == AIState.StalkingPlayer) {
+            _target = GameObject.FindGameObjectWithTag("Player").transform;            
+            transform.position = Vector3.MoveTowards(transform.position, _target.position, Time.deltaTime * _moveSpeed);            
+        }
     }
 }
