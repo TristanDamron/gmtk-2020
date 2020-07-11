3080f343b03008d1d13da250b683ea3c844f2ffe
tdamron <TristanDamron31@gmail.com>
2020-07-11 02:59:27 -0700

    Blackout w/ restart

--- Assets/Scripts/Blackout.cs
+++ Assets/Scripts/Blackout.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Blackout : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class Blackout : MonoBehaviour
     {
         if (GameManager.blackOut) {
             _blackOutPanel.color = Color.Lerp(_blackOutPanel.color, Color.black, Time.deltaTime * 2f);
+            Invoke("Restart", 2f);
         }
+    }
+
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.blackOut = false;
     }
 }
